24be14f5d9fa84a334f83a095bca2024e7d473ba
Izel Nakri <contact@izelnakri.com>
2018-07-26 23:44:01 +0200

    v0.5.5 build process never stops

---
+++ test/runners/application-files-watcher-test.js
@@ -0,0 +1,67 @@
+// import fs from 'fs-extra';
+import test from 'ava';
+// import WebSocket from 'ws';
+// import applicationFilesWatcher from '../../lib/runners/application-files-watcher';
+//
+// // test.beforeEach(async () => {
+// //   await fs.remove('dummyapp');
+// // });
+// //
+// // test.afterEach.always(async () => {
+// //   await fs.remove('dummyapp');
+// // });
+//
+// const DEFAULT_SOCKET_PORT = 65511;
+//
+// // TODO: watching should work without websocket connection
+//
+test('it watches correctly on development mode', async (t) => {
+  t.true(true);
+  // TODO: mock CWD
+  // TODO: get stdout here as array
+
+  // applicationFilesWatcher();
+  // const firstSocket = new WebSocket(`ws://localhost:${65511}`);
+  // const secondSocket = new WebSocket(`ws://localhost:${65511}`);
+
+  // fs.
+  // it works , count stdout strings for building built etc,
+  // test event colors(do 3 different events)
+  // test broadcasts (only one watch event even in multiple connections) (NOTE: always do this one for every test case)
+});
+//
+// // test('it watches memserver files correctly', async (t) => {
+// //
+// // });
+//
+// // test('it watches test files correctly', async (t) => {
+// //
+// // });
+//
+// // test('it can run on different sockerPort and endpoint', async (t) => {
+// //
+// // });
+//
+// // test('it handles css, js, hbs syntax errors gracefully on fastboot', async (t) => {
+// //
+// // });
+//
+// // test('it handles css, js, hbs syntax errors gracefully without fastboot', async (t) => {
+// //
+// // });
+//
+//
+// // it works , count stdout strings for building built etc,
+// // test event colors(do 3 different events)
+// // test broadcasts (only one watch event even in multiple connections) (NOTE: always do this one for every test case)
+//
+// // it watches test files correctly
+// // it watches memserver files correctly
+// // it reloads fastboot correctly
+// // it can have a different socketPort
+// // it can change the ENV by changing the environment.js
+// // do the graceful error handling for (hbs, js, css) here too
+//
+//
+//
+// // TODO: also test start-http-server, then transpilers
