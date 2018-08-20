9a7ba039ed6566255fd03397c92af5304a9ab6a4
Izel Nakri <contact@izelnakri.com>
2018-08-20 18:07:00 +0200

    boilerplate eslint errors fixed

--- ember-app-boilerplate/memserver/server.js
+++ ember-app-boilerplate/memserver/server.js
@@ -1,6 +1,6 @@
 import ENV from '../config/environment';
 
-export default function(Models) {
+export default function(Models) { // eslint-disable-line
   this.urlPrefix = ENV.APP ? ENV.APP.API_HOST : 'http://localhost:3000';
 
   // this.get('/users/:id', (request) => {
