a4baaf7ee30eacb0e815858f30fbe2fa4cd0e5b8
Izel Nakri <contact@izelnakri.com>
2018-09-03 01:16:29 +0200

    lib code moves to mber library

--- ember-app-boilerplate/documentation/ui/routes/route.js
+++ ember-app-boilerplate/documentation/ui/routes/route.js
@@ -1,5 +1,3 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({});
-
-// NOTE: by default should show color scheme, typography(titles), text colors, background colors, icons, buttons, alerts
