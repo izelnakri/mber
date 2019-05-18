79cbe8166bc381fd31b02037ad32432cb6f272cf
Izel Nakri <contact@izelnakri.com>
2019-05-18 16:14:32 +0300

    important jquery_integration disabling

--- ember-app-boilerplate/config/environment.js
+++ ember-app-boilerplate/config/environment.js
@@ -29,6 +29,7 @@ module.exports = function(environment) {
       enabled: ['demo', 'test', 'memserver'].includes(environment)
     },
     EmberENV: {
+      _JQUERY_INTEGRATION: false,
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
