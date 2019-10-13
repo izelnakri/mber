2129d7351f86cb081a76c7b385b850248541eaf0
Izel Nakri <contact@izelnakri.com>
2019-10-13 16:05:46 +0200

    convert-es-module-to-amd unit test added

---
+++ test/transpilers/convert-es-module-to-amd-test.js
@@ -0,0 +1,61 @@
+import test from 'ava';
+import convertESModuletoAMD from '../../lib/transpilers/convert-es-module-to-amd.js';
+
+test('importAddonToAmd(moduleName) works', async (t) => {
+  const code = `import EmberRouter from '@ember/routing/router';
+import DocumentationRouter from 'mber-documentation';
+import ENV from '../config/environment';
+
+const Router = EmberRouter.extend({
+  location: ENV.locationType,
+  rootURL: ENV.rootURL
+});
+
+Router.map(function() {
+  this.route('index', { path: '/' });
+
+  if (ENV.documentation && ENV.documentation.enabled) {
+    DocumentationRouter.apply(this, [ENV]);
+  }
+
+  this.route('not-found', { path: '/*path' });
+});
+
+export default Router;`;
+
+  t.true(
+    (await convertESModuletoAMD(code, { moduleName: 'src/router' })).trim() ===
+      `define("src/router", ["exports", "mber-documentation", "config/environment"], function (_exports, _mberDocumentation, _environment) {
+  "use strict";
+
+  Object.defineProperty(_exports, "__esModule", {
+    value: true
+  });
+  _exports.default = void 0;
+  const Router = Ember.Router.extend({
+    location: _environment.default.locationType,
+    rootURL: _environment.default.rootURL
+  });
+  Router.map(function () {
+    this.route('index', {
+      path: '/'
+    });
+
+    if (_environment.default.documentation && _environment.default.documentation.enabled) {
+      _mberDocumentation.default.apply(this, [_environment.default]);
+    }
+
+    this.route('not-found', {
+      path: '/*path'
+    });
+  });
+  var _default = Router;
+  _exports.default = _default;
+});`
+  );
+});
+
+// test.todo('importAddonToAmd(moduleName, addonPath) works for node_module addon folder');
+// test.todo('importAddonToAmd(moduleName, addonPath) works for non-node_module folder');
+
+// TODO: test behavior   const packagePath = fs.existsSync(mberPackage) ? mberPackage : `${projectRoot}/node_modules/${addonPath}`;
