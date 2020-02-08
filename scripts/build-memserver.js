68bf1cc30f135aa2ee7ee54e3d60029d7cc69523
Izel Nakri <contact@izelnakri.com>
2020-02-08 21:13:09 +0100

    memserver v2 now works for both for fastboot and non-fastboot

--- scripts/build-memserver.js
+++ scripts/build-memserver.js
@@ -3,11 +3,11 @@ import findProjectRoot from '../lib/utils/find-project-root.js';
 import convertESModuletoAMD from '../lib/transpilers/convert-es-module-to-amd.js';
 import transpileNPMImport from '../lib/transpilers/transpile-npm-imports.js';
 
-// const removeFetch = `
-//   (function() {
-//     window.fetch = undefined;
-//   })();
-// `;
+const removeFetch = `
+  (function() {
+    window.fetch = undefined;
+  })();
+`;
 
 async function build() {
   const PROJECT_PATH = await findProjectRoot();
@@ -15,10 +15,10 @@ async function build() {
   const VENDOR_PATH = `${PROJECT_PATH}/vendor`;
 
   return Promise.all([
-    // fs.readFile(`${MODULE_PATH}/whatwg-fetch/dist/fetch.umd.js`),
+    fs.readFile(`${MODULE_PATH}/whatwg-fetch/dist/fetch.umd.js`),
     transpileNPMImport('memserver/model', `${MODULE_PATH}/memserver/model.js`),
     transpileNPMImport('memserver/server', `${MODULE_PATH}/memserver/server.js`)
-  ]).then(async ([memServerModelModule, memServerServerModule]) => {
+  ]).then(async ([fetchReplacement, memServerModelModule, memServerServerModule]) => {
     const memserverResponseModule = await convertESModuletoAMD(`
       export default function(statusCode=200, data={}, headers={}) {
         return [
@@ -29,10 +29,13 @@ async function build() {
       }
     `, { moduleName: 'memserver/response' });
 
-    // NOTE: cant remove fetch replacements because ember-fetch injection doesnt take into account the possiblity of fetch mocking in node.js
     return Promise.all([
       fs.copy(`${PROJECT_PATH}/scripts/memserver/initializers/ajax.js`, `${VENDOR_PATH}/memserver/fastboot/initializers/ajax.js`),
       fs.writeFile(`${VENDOR_PATH}/memserver.js`, `
+        ${removeFetch}
+
+        ${fetchReplacement}
+
         ${memServerModelModule}
 
         ${memserverResponseModule}
