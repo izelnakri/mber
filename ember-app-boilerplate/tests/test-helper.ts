c305a09d4a1fe41af9dcbc2c8b352b0b6d3d5761
Izel Nakri <contact@izelnakri.com>
2020-12-10 00:07:22 +0100

    remove redundant eslint type lint from boilerplate

--- ember-app-boilerplate/tests/test-helper.ts
+++ ember-app-boilerplate/tests/test-helper.ts
@@ -1,19 +1,3 @@
-declare module "ember-test-helpers" {
-  interface Element {
-    querySelector: (selectorText: string) => Element;
-    value: any;
-    textContent: any;
-    type: any;
-    required: any;
-    getAttribute: any;
-    autofocus: any;
-    previousElementSibling: any;
-    nextElementSibling: any;
-    placeholder: any;
-    [propName: string]: any;
-  }
-}
-
 import 'qunit-dom';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
